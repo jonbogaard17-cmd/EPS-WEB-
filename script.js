36812739a2b105fd528ddb64ded85a127fb852c7
jonbogaard17-cmd <jonbogaard17@gmail.com>
2026-03-29 23:56:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+// script.js
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    if (this.getAttribute('href') !== '#') {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+        target.scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
+    }
+  });
+});
